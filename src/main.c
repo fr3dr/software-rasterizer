5cb36a7d5696d0b0eef6d89f95fda938676ec690
fr3dr <46770388+fr3dr@users.noreply.github.com>
2026-09-18 11:16:36 -0700

    added diffuse lighting and parsing vertex normals

--- src/main.c
+++ src/main.c
@@ -46,19 +46,38 @@ typedef struct {
     uint32_t color;
 } face;
 
+// TODO: support for face normals
 typedef struct {
     vec3 *vertices;
     size_t vertices_len;
+    vec3 *vertex_normals;
+    size_t vertex_normals_len;
     face *faces;
     size_t faces_len;
     vec3 pos;
     rotation rot;
 } model;
 
-int parse_obj(const char *filename, vec3 **vertices, size_t *vertices_len, face **faces, size_t *faces_len) {
+typedef struct {
+    vec3 pos;
+    double r;
+    double g;
+    double b;
+} light;
+
+light global_lights[] = {
+    {.pos = {-4, 4, 4}, .r = 1.0, .g = 0.98, .b = 0.93},
+    {.pos = {4, 10, 4}, .r = 1.0, .g = 0, .b = 1.0},
+};
+
+int parse_obj(const char *filename, vec3 **vertices, size_t *vertices_len, vec3 **vertex_normals, size_t *vertex_normals_len, face **faces, size_t *faces_len) {
     size_t vertices_size = 128;
     size_t vertices_pos = 0;
     *vertices = malloc(vertices_size * sizeof(vec3));
+
+    size_t vertex_normals_size = 128;
+    size_t vertex_normals_pos = 0;
+    *vertex_normals = malloc(vertex_normals_size * sizeof(vec3));
 
     size_t faces_size = 128;
     size_t faces_pos = 0;
@@ -68,7 +87,7 @@ int parse_obj(const char *filename, vec3 **vertices, size_t *vertices_len, face 
     char *line = NULL;
     size_t line_len = 0;
     while (getline(&line, &line_len, file) != -1) {
-        if (line_len == 0 || ((line[0] != 'v' || line[1] != ' ') && line[0] != 'f')) {
+        if (line_len == 0 || (line[0] != 'v' && line[0] != 'f')) {
             continue;
         }
 
@@ -78,12 +97,21 @@ int parse_obj(const char *filename, vec3 **vertices, size_t *vertices_len, face 
             double y = strtof(end_ptr + sizeof(char), &end_ptr);
             double z = strtof(end_ptr + sizeof(char), &end_ptr);
 
-            if (vertices_pos >= vertices_size) {
-                vertices_size *= 2;
-                *vertices = realloc(*vertices, vertices_size * sizeof(vec3));
+            if (line[1] == ' ') {
+                if (vertices_pos >= vertices_size) {
+                    vertices_size *= 2;
+                    *vertices = realloc(*vertices, vertices_size * sizeof(vec3));
+                }
+                (*vertices)[vertices_pos] = (vec3){x, y, z};
+                vertices_pos++;
+            } else if (line[1] == 'n') {
+                if (vertex_normals_pos >= vertex_normals_size) {
+                    vertex_normals_size *= 2;
+                    *vertex_normals = realloc(*vertex_normals, vertex_normals_size * sizeof(vec3));
+                }
+                (*vertex_normals)[vertex_normals_pos] = (vec3){x, y, z};
+                vertex_normals_pos++;
             }
-            (*vertices)[vertices_pos] = (vec3){x, y, z};
-            vertices_pos++;
         } else if (line[0] == 'f') {
             if (faces_pos >= faces_size) {
                 faces_size *= 2;
@@ -103,7 +131,8 @@ int parse_obj(const char *filename, vec3 **vertices, size_t *vertices_len, face 
             // model needs to have triangle faces
             assert(index == 3);
 
-            (*faces)[faces_pos].color = 0xFF000000 | (random() % 0x00FFFFFF);
+            // (*faces)[faces_pos].color = 0xFF000000 | (random() % 0x00FFFFFF);
+            (*faces)[faces_pos].color = 0xFFFFFFFF;
             (*faces)[faces_pos].count = index;
             faces_pos++;
         }
@@ -112,9 +141,19 @@ int parse_obj(const char *filename, vec3 **vertices, size_t *vertices_len, face 
     free(line);
 
     *vertices_len = vertices_pos;
+    *vertex_normals_len = vertex_normals_pos;
+    printf("vertices count: %zu, vertex normals count: %zu\n", *vertices_len, *vertex_normals_len);
+    // assert((*vertices_len) == (*vertex_normals_len));
     *faces_len = faces_pos;
 
     return 0;
+}
+
+
+/// vec3 util functions
+static inline vec3 vec3_norm(vec3 vec) {
+    double mag = sqrt(vec.x * vec.x + vec.y * vec.y + vec.z * vec.z);
+    return (vec3){vec.x / mag, vec.y / mag, vec.z / mag};
 }
 
 
@@ -165,7 +204,7 @@ vec3 apply_camera(camera camera, vec3 vertex) {
 }
 
 
-vec3 clip_line(vec3 behind, vec3 infront) {
+static inline vec3 clip_line(vec3 behind, vec3 infront) {
     double x_dist = infront.x - behind.x;
     double y_dist = infront.y - behind.y;
     double z_dist = infront.z - behind.z;
@@ -175,7 +214,7 @@ vec3 clip_line(vec3 behind, vec3 infront) {
     return (vec3){behind.x + x_dist * percent, behind.y + y_dist * percent, behind.z + z_dist * percent};
 }
 
-double signed_triangle_area(vec3 a, vec3 b, vec3 c) {
+static inline double signed_triangle_area(vec3 a, vec3 b, vec3 c) {
     return 0.5 * ((a.x - c.x) * (b.y - a.y) - (a.x - b.x) * (c.y - a.y));
 }
 
@@ -241,7 +280,7 @@ int clip_triangle(vec3 *v1r, vec3 *v2r, vec3 *v3r, vec3 *nv1, vec3 *nv2, vec3 *n
     return 0;
 }
 
-void draw_triangle(vec3 v1, vec3 v2, vec3 v3, uint32_t color) {
+void draw_triangle(vec3 v1, vec3 v2, vec3 v3, vec3 vn1, vec3 vn2, vec3 vn3, vec3 v1w, vec3 v2w, vec3 v3w, uint32_t color) {
     double x_min = fmin(fmin(v1.x, v2.x), v3.x);
     double x_max = fmax(fmax(v1.x, v2.x), v3.x);
     double y_min = fmin(fmin(v1.y, v2.y), v3.y);
@@ -253,35 +292,66 @@ void draw_triangle(vec3 v1, vec3 v2, vec3 v3, uint32_t color) {
 
     double face_area = signed_triangle_area(v1, v2, v3);
 
-    for (int y = y_min; y < y_max; y++) {
-        if (y < 0)       continue;
-        if (y >= HEIGHT) break;
-        for (int x = x_min; x < x_max; x++) {
-            if (x < 0)      continue;
-            if (x >= WIDTH) break;
+    for (int screen_y = y_min; screen_y < y_max; screen_y++) {
+        if (screen_y < 0)       continue;
+        if (screen_y >= HEIGHT) break;
+        for (int screen_x = x_min; screen_x < x_max; screen_x++) {
+            if (screen_x < 0)      continue;
+            if (screen_x >= WIDTH) break;
 
-            vec3 point = {x, y, 1};
+            // get barycentric coordinates
+            vec3 point = {screen_x, screen_y, 1};
             double alpha = signed_triangle_area(point, v2, v3) / face_area;
             double beta = signed_triangle_area(point, v3, v1) / face_area;
             double gamma = signed_triangle_area(point, v1, v2) / face_area;
             if (alpha < 0 || beta < 0 || gamma < 0) {
                 continue;
             }
+
+            // z-buffering
             double z = 1 / (1 / v1.z * alpha + 1 / v2.z * beta + 1 / v3.z * gamma);
-
-            int index = y * WIDTH + x;
-            if (z < z_buffer[index]) {
-                z_buffer[index] = z;
-
-                double percent = 1.0 - (z - NEAR_CLIPPING_PLANE) / (10 - NEAR_CLIPPING_PLANE);
-                if (percent < 0) percent = 0;
-                uint8_t r = (uint8_t)(color >> 16) * percent;
-                uint8_t g = (uint8_t)(color >> 8) * percent;
-                uint8_t b = (uint8_t)(color) * percent;
-                uint32_t clr = 0xFF000000 | r | (g << 8) | (b << 16);
-                wwl_draw_pixel(state, x, y, clr);
+            int index = screen_y * WIDTH + screen_x;
+            if (z >= z_buffer[index]) {
+                continue;
             }
+            z_buffer[index] = z;
 
+            vec3 normal;
+            normal.x = vn1.x * alpha + vn2.x * beta + vn3.x * gamma;
+            normal.y = vn1.y * alpha + vn2.y * beta + vn3.y * gamma;
+            normal.z = vn1.z * alpha + vn2.z * beta + vn3.z * gamma;
+            normal = vec3_norm(normal);
+
+            // lighting
+            // TODO: optimize lighting calculations
+            vec3 frag_pos;
+            frag_pos.x = v1w.x * alpha + v2w.x * beta + v3w.x * gamma;
+            frag_pos.y = v1w.y * alpha + v2w.y * beta + v3w.y * gamma;
+            frag_pos.z = v1w.z * alpha + v2w.z * beta + v3w.z * gamma;
+            double light_r = 0;
+            double light_g = 0;
+            double light_b = 0;
+            int light_count = sizeof(global_lights) / sizeof(global_lights[0]);
+            for (int i = 0; i < light_count; i++) {
+                vec3 light_pos = global_lights[i].pos;
+                vec3 light_dir = vec3_norm((vec3){light_pos.x - frag_pos.x, light_pos.y - frag_pos.y, light_pos.z - frag_pos.z});
+                double light_dot = light_dir.x * normal.x + light_dir.y * normal.y + light_dir.z * normal.z;
+                double ambient_light = 0.1;
+                double diffuse = light_dot > 0 ? light_dot : 0;
+                double light_intensity = (ambient_light + diffuse) <= 1 ? (ambient_light + diffuse) : 1;
+                light_r += global_lights[i].r * light_intensity;
+                light_g += global_lights[i].g * light_intensity;
+                light_b += global_lights[i].b * light_intensity;
+            }
+            if (light_r > 1) light_r = 1;
+            if (light_g > 1) light_g = 1;
+            if (light_b > 1) light_b = 1;
+
+            uint8_t r = (uint8_t)(color >> 16) * light_r;
+            uint8_t g = (uint8_t)(color >> 8) * light_g;
+            uint8_t b = (uint8_t)(color) * light_b;
+            uint32_t pixel_color = 0xFF000000 | r | (g << 8) | (b << 16);
+            wwl_draw_pixel(state, screen_x, screen_y, pixel_color);
         }
     }
 }
@@ -292,13 +362,15 @@ model create_model_from_obj(const char *filename) {
     model model = {
         .vertices = NULL,
         .vertices_len = 0,
+        .vertex_normals = NULL,
+        .vertex_normals_len = 0,
         .faces = NULL,
         .faces_len = 0,
         .pos = {0, 0, 0},
         .rot = {0, 0, 0},
     };
 
-    parse_obj(filename, &model.vertices, &model.vertices_len, &model.faces, &model.faces_len);
+    parse_obj(filename, &model.vertices, &model.vertices_len, &model.vertex_normals, &model.vertex_normals_len, &model.faces, &model.faces_len);
 
     return model;
 }
@@ -316,22 +388,37 @@ void render_model(model model, camera camera) {
     for (size_t i = 0; i < model.faces_len; i++) {
         face face = model.faces[i];
 
-        vec3 v1 = apply_camera(camera, transform(rotate(model.vertices[face.indices[0]], model.rot), model.pos));
-        vec3 v2 = apply_camera(camera, transform(rotate(model.vertices[face.indices[1]], model.rot), model.pos));
-        vec3 v3 = apply_camera(camera, transform(rotate(model.vertices[face.indices[2]], model.rot), model.pos));
+        // TODO: frustum culling
 
+        // get vertex normals
+        vec3 vn1 = rotate(model.vertex_normals[face.indices[0]], model.rot);
+        vec3 vn2 = rotate(model.vertex_normals[face.indices[1]], model.rot);
+        vec3 vn3 = rotate(model.vertex_normals[face.indices[2]], model.rot);
+
+        // get vertex world positions
+        vec3 v1w = transform(rotate(model.vertices[face.indices[0]], model.rot), model.pos);
+        vec3 v2w = transform(rotate(model.vertices[face.indices[1]], model.rot), model.pos);
+        vec3 v3w = transform(rotate(model.vertices[face.indices[2]], model.rot), model.pos);
+
+        // get vertex view space positions
+        vec3 v1v = apply_camera(camera, v1w);
+        vec3 v2v = apply_camera(camera, v2w);
+        vec3 v3v = apply_camera(camera, v3w);
+
+        // get the clipped vertices
+        // TODO: also get the new triangles vertex normals
         vec3 nv1 = {0};
         vec3 nv2 = {0};
         vec3 nv3 = {0};
-        int clipped = clip_triangle(&v1, &v2, &v3, &nv1, &nv2, &nv3);
-
+        int clipped = clip_triangle(&v1v, &v2v, &v3v, &nv1, &nv2, &nv3);
         if (clipped == 3) {
             continue;
         };
 
-        vec3 v1s = screen(project(v1));
-        vec3 v2s = screen(project(v2));
-        vec3 v3s = screen(project(v3));
+        // get vertex screen space positions
+        vec3 v1s = screen(project(v1v));
+        vec3 v2s = screen(project(v2v));
+        vec3 v3s = screen(project(v3v));
 
         // backface culling
         vec3 v1v2 = {v2s.x - v1s.x, v2s.y - v1s.y, v2s.z - v1s.z};
@@ -344,14 +431,15 @@ void render_model(model model, camera camera) {
             continue;
         }
 
+        // draw clipped triangle
         if (clipped == 1) {
             vec3 v1 = screen(project(nv1));
             vec3 v2 = screen(project(nv2));
             vec3 v3 = screen(project(nv3));
-            draw_triangle(v1, v2, v3, face.color);
+            draw_triangle(v1, v2, v3, vn1, vn2, vn3, v1w, v2w, v3w, face.color);
         }
 
-        draw_triangle(v1s, v2s, v3s, face.color);
+        draw_triangle(v1s, v2s, v3s, vn1, vn2, vn3, v1w, v2w, v3w, face.color);
     }
 }
 
@@ -363,6 +451,7 @@ int main(void) {
     wwl_set_max_size(state, WIDTH, HEIGHT);
 
     bool mouse_control = false;
+    double light_angle = 0;
 
     camera camera = {
         .pos = {0, 0, 0},
@@ -384,8 +473,11 @@ int main(void) {
 
     while (wwl_update(state)) {
         double dt = wwl_get_deltatime(state);
-        printf("fps: %f\n", 1 / dt);
-        printf("frametime: %f\n", dt);
+        printf("fps: %f, frametime: %f\n", 1 / dt, dt);
+
+        light_angle += dt;
+        global_lights[0].pos.x = cos(light_angle) * 3.0;
+        global_lights[0].pos.z = 5 + sin(light_angle) * 3.0;
 
         if (wwl_is_key_down(state, KEY_SPACE)) {
             camera.pos.y += MOVE_SPEED * dt;
@@ -445,7 +537,7 @@ int main(void) {
             }
         }
 
-        wwl_clear_background(state, 0xFF608060);
+        wwl_clear_background(state, 0xFF000000);
         for (int i = 0; i < WIDTH * HEIGHT; i++) {
             z_buffer[i] = FAR_CLIPPING_PLANE;
         }
